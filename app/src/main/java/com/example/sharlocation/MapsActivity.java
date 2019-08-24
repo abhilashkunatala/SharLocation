456b7d4e4881f7305a3f851e68905047a216d24b
Abhilash Kuntala <201651002@iiitvadodara.ac.in>
2019-08-25 00:34:43 +0530

    taking the number input from user

--- app/src/main/java/com/example/sharlocation/MapsActivity.java
+++ app/src/main/java/com/example/sharlocation/MapsActivity.java
@@ -1,16 +1,27 @@
 package com.example.sharlocation;
 
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.internal.StringResourceValueReader;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,14 +29,27 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private LocationManager locationManager;
     private LocationListener locationListener;
-    private final long MIN_TIME=100000;// time interval after update the location it is milliseconds
-    private final long MIN_DIST= 1000;// distance after update the location
+    private final long MIN_TIME=1000;// time interval to update the location it is milliseconds
+    private final long MIN_DIST= 10;// distance after update the location
     private LatLng  latLng;// latitude and longitude
+    public Button button;
+    String message1 = "";
+  //  private LatLng latLng1;
+    TextView textView;
+   // public String phoneNumber;
+    //int reuestCode = 10000;
+
+    SearchView searchView;
+    SupportMapFragment mapFragment;
+
 
 
     @Override
@@ -42,10 +66,63 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED );
 
+
+            searchView =(SearchView)findViewById(R.id.sv_location);
+            mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+            textView = (TextView)findViewById(R.id.textview);
+
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    String location = searchView.getQuery().toString();
+                    List<Address> addressList = null;
+                    if(location != null || !location.equals("")){
+                        Geocoder geocoder = new Geocoder(MapsActivity.this);
+                        try{
+                            addressList = geocoder.getFromLocationName(location,1);
+                        }
+
+                            catch (IOException e){
+                                    e.printStackTrace();
+                            }
+
+                        Address address= addressList.get(0);
+                        LatLng latLng =new LatLng(address.getLatitude(),address.getLongitude());
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10  ));
+
+
+                     }
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    return false;
+                }
+            });
+
+
+            mapFragment.getMapAsync(this);
+
+
+
+
+
+        button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                hello();
+
+
+            }
+        });
+
     }
 
-
-    /**
+/*
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
@@ -68,15 +145,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onLocationChanged(Location location) {
 
                 try{
-                        latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                        latLng= new LatLng(location.getLatitude(),location.getLongitude());
                     mMap.addMarker(new MarkerOptions().position(latLng).title("MY Postion "));// going to to new lat and longi that is moving camera
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
-                    //
-                    String phoneNumber = "6302506749";
+
+                    String phoneNumber =message1;
+                   
                     String  myLatitude= String.valueOf(location.getLatitude());
                     String myLongitude = String.valueOf(location.getLongitude());
-                    String message = " Latitude = "+ myLatitude+" "+"longitude="+myLongitude+"This is Abhilash kuntala";
+                    String message = " Latitude = "+ myLatitude+" "+"longitude="+myLongitude+ "This is Abhilash Kuntala";
                     SmsManager smsManager =  SmsManager.getDefault();
                     smsManager.sendTextMessage(phoneNumber,null, message,null,null);
 
@@ -109,9 +187,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
         try{
-
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,MIN_TIME,MIN_DIST,locationListener);
-           locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,MIN_TIME,MIN_DIST,locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,MIN_TIME,MIN_DIST,locationListener);
 
 
         }
@@ -119,6 +196,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         catch(SecurityException e){
             e.printStackTrace();
         }
+
+    }
+
+    public  void hello()
+    {
+        Intent intent = new Intent(this,Message1.class);
+        startActivityForResult(intent ,5);
+
+      // mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if(requestCode==5){
+            message1 = data.getStringExtra("key");
+            textView.setText(message1);
+//            phoneNumber.matches(message);
+
+
+            Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show();
+        }
+
+
+
 
     }
 }
